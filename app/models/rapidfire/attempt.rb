f2bdbba9c1c2f67162cc950916d2de98d01fa368
Siobhan Jacobson <siobhan.jacobson@gmail.com>
2019-07-25 14:33:27 -0400

    Do not allow a user to take a survey more than once.

--- app/models/rapidfire/attempt.rb
+++ app/models/rapidfire/attempt.rb
@@ -4,6 +4,8 @@ module Rapidfire
     belongs_to :user, polymorphic: true
     has_many   :answers, inverse_of: :attempt, autosave: true
 
+    validates_uniqueness_of :user, scope: :survey
+
     if Rails::VERSION::MAJOR == 3
       attr_accessible :survey, :user
     end
